c834479d0e64d66a12006bbaa172a8f097a912c0
dpchamps <me@davecampion.com>
2025-10-21 23:03:04 -0700

    fix(package): remove generated javascript files

--- packages/sylph-jsx/src/engine/libs/Math.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.lerp = void 0;
-var lerp = function (start, end, percentage) {
-    return start * (1 - percentage) + end * percentage;
-};
-exports.lerp = lerp;
--- packages/sylph-jsx/src/utility-numbers.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.clamp = void 0;
-var clamp = function (min, max, value) {
-    return Math.max(min, Math.min(max, value));
-};
-exports.clamp = clamp;
