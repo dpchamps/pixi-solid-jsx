aba16bf91f0897123e3be42e63a32c51a336ba8a
dpchamps <me@davecampion.com>
2025-10-25 22:46:09 -0700

    chore(sylph-jsx): make even less scary

--- packages/sylph-jsx/src/engine/effects/input/create-pointer/pointer-impl.ts
+++ packages/sylph-jsx/src/engine/effects/input/create-pointer/pointer-impl.ts
@@ -14,117 +14,42 @@ import type {
   EventSubscriber,
 } from "./types.js";
 import {
-  createEventKey,
-  createPointerEventData,
-  isDuplicateMouseEvent,
+  createPointerEventState,
+  LISTENER_OPTIONS,
+  POINTER_EVENTS,
+  type PointerEventState,
 } from "./utilities-pointer.js";
 
-const POINTER_EVENTS = [
-  "pointerdown",
-  "pointerup",
-  "pointermove",
-  "pointercancel",
-  "pointerover",
-  "pointerout",
-  "pointerenter",
-  "pointerleave",
-  "gotpointercapture",
-  "lostpointercapture",
-] as const;
-
-const createPointerEventState = () => {
-  const eventBuffer: PointerEventData[] = [];
-  const eventSubscriptions = new Set<EventSubscriber>();
-  const seenEvents = new Set<string>();
-  let hadEventsLastFrame = false;
-
-  return {
-    isEmptyEventQueue: () =>
-      eventSubscriptions.size === 0 ||
-      (eventBuffer.length === 0 && !hadEventsLastFrame),
-    shouldNotifySubscriptionsOfNoEvents: () =>
-      eventBuffer.length === 0 && hadEventsLastFrame,
-    performEmptyEventNotification: () => {
-      // When there were no new events, we should notify subscribers that no events occured
-      hadEventsLastFrame = false;
-      eventSubscriptions.forEach((subscriber) => {
-        subscriber.setValue([]);
-      });
-    },
-    performEmptyQueueEventAction: (forceClean: boolean = false) => {
-      if (eventSubscriptions.size === 0 || forceClean) {
-        hadEventsLastFrame = false;
-        eventBuffer.length = 0;
-        eventSubscriptions.clear();
-        seenEvents.clear();
-      }
-    },
-    performStandardEventNotificationAction: () => {
-      hadEventsLastFrame = true;
-      eventSubscriptions.forEach((subscriber) => {
-        const matchedEvents = eventBuffer.filter((event) => {
-          const typeMatches = subscriber.eventTypes.has(event.eventType);
-          const filterMatches = subscriber.filter
-            ? subscriber.filter(event)
-            : true;
-          return typeMatches && filterMatches;
-        });
-
-        subscriber.setValue(matchedEvents);
-      });
-
-      eventBuffer.length = 0;
-      seenEvents.clear();
-    },
-    addEventData: (event: PointerEvent, type: PointerEventType) => {
-      const eventKey = createEventKey(event);
-      const eventData = createPointerEventData(event, type);
-      if (isDuplicateMouseEvent(event, eventKey, seenEvents)) return;
-      seenEvents.add(eventKey);
-      eventBuffer.push(eventData);
-    },
-    addSubscription: (eventSubscription: EventSubscriber) => {
-      eventSubscriptions.add(eventSubscription);
-    },
-    removeSubscription: (eventSubscription: EventSubscriber) => {
-      eventSubscriptions.delete(eventSubscription);
-    },
-  };
-};
+const createEventFunctionsWithPointerEventState = (
+  pointerEventState: PointerEventState,
+) =>
+  POINTER_EVENTS.map(
+    (eventType) =>
+      [
+        eventType,
+        (event: PointerEvent) =>
+          pointerEventState.addEventData(event, eventType),
+      ] as const,
+  );
 
 export const createPointerImpl = (element: PointerLikeEl): Pointer => {
-  const listenerOptions = { passive: true } satisfies AddEventListenerOptions;
   const pointerEventState = createPointerEventState();
-
-  const createEventFn =
-    <T extends PointerEventType>(eventType: T) =>
-    (event: PointerEvent) =>
-      pointerEventState.addEventData(event, eventType);
-
-  const eventFns = POINTER_EVENTS.map(
-    (eventType) => [eventType, createEventFn(eventType)] as const,
-  );
+  const eventFns = createEventFunctionsWithPointerEventState(pointerEventState);
 
   createComputed(() => {
     eventFns.forEach(([eventType, fn]) => {
-      element.addEventListener(eventType, fn, listenerOptions);
+      element.addEventListener(eventType, fn, LISTENER_OPTIONS);
     });
   });
 
   onCleanup(() => {
     eventFns.forEach(([eventType, fn]) => {
-      element.removeEventListener(eventType, fn, listenerOptions);
+      element.removeEventListener(eventType, fn, LISTENER_OPTIONS);
     });
-    pointerEventState.performEmptyQueueEventAction(true);
+    pointerEventState.reset();
   });
 
-  onEveryFrame(() => {
-    if (pointerEventState.isEmptyEventQueue())
-      return pointerEventState.performEmptyQueueEventAction();
-    if (pointerEventState.shouldNotifySubscriptionsOfNoEvents())
-      return pointerEventState.performEmptyEventNotification();
-    pointerEventState.performStandardEventNotificationAction();
-  });
+  onEveryFrame(pointerEventState.updateAndTransition);
 
   const onPointerEvent = (
     eventTypes: PointerEventType | PointerEventType[],
--- packages/sylph-jsx/src/engine/effects/input/create-pointer/utilities-pointer.ts
+++ packages/sylph-jsx/src/engine/effects/input/create-pointer/utilities-pointer.ts
@@ -1,4 +1,5 @@
 import type {
+  EventSubscriber,
   PointerEventData,
   PointerEventType,
   PointerState,
@@ -49,6 +50,7 @@ export const createPointerEventData = (
 });
 
 type EventKey = ReturnType<typeof createEventKey>;
+
 export const createEventKey = (event: PointerEvent) =>
   `${event.type}-${event.pointerId}-${event.timeStamp}` as const;
 
@@ -63,3 +65,102 @@ export const isDuplicateMouseEvent = (
 
   return seenEvents.has(key);
 };
+
+export type PointerEventState = ReturnType<typeof createPointerEventState>;
+
+export const createPointerEventState = () => {
+  const eventBuffer: PointerEventData[] = [];
+  const eventSubscriptions = new Set<EventSubscriber>();
+  const seenEvents = new Set<string>();
+  let hadEventsLastFrame = false;
+
+  const reset = () => {
+    hadEventsLastFrame = false;
+    eventBuffer.length = 0;
+    eventSubscriptions.clear();
+    seenEvents.clear();
+  };
+
+  const canTransitionIntoEmptyStateAction = () =>
+    eventSubscriptions.size === 0 ||
+    (eventBuffer.length === 0 && !hadEventsLastFrame);
+
+  const canTransitionIntoEmptyEventNotificationAction = () =>
+    eventBuffer.length === 0 && hadEventsLastFrame;
+
+  const performEmptyStateEventAction = () => {
+    if (eventSubscriptions.size === 0) {
+      reset();
+    }
+  };
+
+  const performEmptyEventNotificationAction = () => {
+    // When there were no new events, we should notify subscribers that no events occured
+    hadEventsLastFrame = false;
+    eventSubscriptions.forEach((subscriber) => {
+      subscriber.setValue([]);
+    });
+  };
+
+  const performDefaultEventNotificationAction = () => {
+    hadEventsLastFrame = true;
+    eventSubscriptions.forEach((subscriber) => {
+      const matchedEvents = eventBuffer.filter((event) => {
+        const typeMatches = subscriber.eventTypes.has(event.eventType);
+        const filterMatches = subscriber.filter
+          ? subscriber.filter(event)
+          : true;
+        return typeMatches && filterMatches;
+      });
+
+      subscriber.setValue(matchedEvents);
+    });
+
+    eventBuffer.length = 0;
+    seenEvents.clear();
+  };
+
+  return {
+    reset: () => reset(),
+    addEventData: (event: PointerEvent, type: PointerEventType) => {
+      const eventKey = createEventKey(event);
+      const eventData = createPointerEventData(event, type);
+      if (isDuplicateMouseEvent(event, eventKey, seenEvents)) return;
+      seenEvents.add(eventKey);
+      eventBuffer.push(eventData);
+    },
+    addSubscription: (eventSubscription: EventSubscriber) => {
+      eventSubscriptions.add(eventSubscription);
+    },
+    removeSubscription: (eventSubscription: EventSubscriber) => {
+      eventSubscriptions.delete(eventSubscription);
+    },
+    updateAndTransition: () => {
+      // If there's no actions to perform then we'll transition into empty state
+      if (canTransitionIntoEmptyStateAction())
+        return performEmptyStateEventAction();
+      // If we can transition into the empty event notification action, we will
+      if (canTransitionIntoEmptyEventNotificationAction())
+        return performEmptyEventNotificationAction();
+      // Other wise perform default
+      performDefaultEventNotificationAction();
+    },
+  };
+};
+
+export const POINTER_EVENTS = [
+  "pointerdown",
+  "pointerup",
+  "pointermove",
+  "pointercancel",
+  "pointerover",
+  "pointerout",
+  "pointerenter",
+  "pointerleave",
+  "gotpointercapture",
+  "lostpointercapture",
+] as const;
+
+export const LISTENER_OPTIONS = {
+  passive: true,
+} satisfies AddEventListenerOptions;
